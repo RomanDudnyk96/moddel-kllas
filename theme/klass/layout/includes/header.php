ad1a7200c61db68f440fd9bd9d6b8247faebc12b
Roman Dudnyk <dudnyk_roman@karpaty-autocenter.com.ua>
2019-01-02 19:01:37 +0200

    first commit

---
+++ theme/klass/layout/includes/header.php
@@ -0,0 +1,88 @@
+<?php
+$surl = new moodle_url('/course/search.php');
+?>
+<header>
+    <div class="row">
+        <div class="column small-6  medium-2">
+            <div class="logo">
+                <a href="#">
+                    <img src="/theme/klass/img/l.svg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="top-menu large-9 large-offset-1 medium-10 columns">
+            <div class="wrap-menu">
+                <div class="row">
+                    <ul class="main-menu columns medium-8 small-12 medium-order-1 small-order-2">
+                        <li><a href="#">Cтупени</a></li>
+                        <li><a href="/theme/klass/about-training.html">Об обучении</a></li>
+                        <li><a href="#">Новости</a></li>
+                        <li><a href="/theme/klass/calendar.html">События</a></li>
+                        <li><a href="#">Библиотека</a></li>
+                    </ul>
+                    <div class="entrance-cabinet columns medium-4 small-12 medium-order-2 small-order-1">
+                        <ul class="entrance-cabinet-right">
+                            <li class="in"><a href="/theme/klass/come-in.html">Войти</a></li>
+                            <li class="to-register"><a href="/theme/klass/registr.html">Зарегистрироваться</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+
+                <ul  class="media media-icon">
+                    <li>
+                        <a href="">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fab fa-vk"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                </ul>
+                <div class="header-contact">
+                    <h4 class="title">
+                        Контакты "Всемирной Общины <br>Санатана Дхармы":
+
+                    </h4>
+
+                    <h5 class="phone">
+                        Трайлокьядеви<br>
+                        +38 097-415-1900
+                    </h5>
+                    <h5 class="email">
+                        <a href="mailto:sadhuloka@gmail.com">
+                            sadhuloka@gmail.com
+                        </a>
+
+                    </h5>
+                </div>
+            </div>
+
+
+        </div>
+        <!--<div class="entrance-cabinet large-3 medium-4 columns">-->
+        <!--<ul>-->
+        <!--<li class="in"><a href="come-in.html">Войти</a></li>-->
+        <!--<li class="to-register"><a href="registr.html">Зарегистрироваться</a></li>-->
+        <!--</ul>-->
+        <!--</div>-->
+        <div class="column small-6">
+            <i class="fa fa-align-right toggle"></i>
+        </div>
+    </div>
+</header>
+
+<!--E.O.Header-->
